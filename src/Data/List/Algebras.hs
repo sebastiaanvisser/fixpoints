e4cbaad1867d9c000da590da7d66b4f59f9bd0e3
Sebastiaan Visser <sfvisser@cs.uu.nl>
2010-05-24 17:18:42 +0200

    Some algebras on lists.

--- src/Data/List/Algebras.hs
+++ src/Data/List/Algebras.hs
@@ -1,40 +1,40 @@
+{-# LANGUAGE TypeOperators, ScopedTypeVariables #-}
 module Data.List.Algebras where
 
--- import Generics.Fixpoint
--- import qualified Data.Morphism.Anamorphism   as Ana
--- import qualified Data.Morphism.Apomorphism   as Apo
--- import qualified Generics.Morphism.Para  as Para
+-- import Data.Annotation
+-- import Data.Monoid
+-- import qualified Data.Morphism.Apomorphism      as Apo
+-- import qualified Data.Morphism.Endoapomorphism  as Endoapo
+-- import qualified Data.Morphism.Paramorphism     as Para
+import Data.Constant
+import Data.Fixpoint
 import Data.List.Abstract
-import Data.Monoid
+import Data.Sum
 import Prelude hiding (tail)
-import qualified Data.Morphism.Catamorphism  as Cata
+import qualified Data.Morphism.Anamorphism      as Ana
+import qualified Data.Morphism.Catamorphism     as Cata
+import qualified Data.Morphism.Endoparamorphism as Endopara
 
-{-fromList :: Ana.Coalg [x] (ListF x)
-fromList = Ana.Phi $ \f ->
-  case f of
-    []   -> Nil
-    x:xs -> Cons x xs
+fromList :: Ana.Coalgebra [x] (ListF x)
+fromList = Ana.Phi $ \f -> case f of [] -> Nil ; x:xs -> Cons x xs
 
-singleton :: Ana.CoalgA a (Maybe x) (ListF x)
-singleton = Ana.Phi $ \f ->
-  case f of
-    Nothing -> Nil
-    Just x  -> Cons x Nothing-}
+singleton :: Ana.Coalgebra (Maybe x) (ListF x)
+singleton = Ana.Phi (maybe Nil (flip Cons Nothing))
 
--- length :: Num n => Cata.Alg (ListF t) n
--- length = Cata.Psi $ \f ->
---   case f of
---     Nil      -> 0
---     Cons _ n -> 1 + n
+length :: Num n => Cata.Algebra (ListF x) n
+length = Cata.Psi (list 0 (const (1+)))
 
-{-cons :: x -> Apo.Coendo (ListF x)
-cons v = Apo.Phi $ Cons v . Left-}
+prepend :: [x] -> Endopara.Algebra (ListF x)
+prepend vs = Endopara.Psi $ \f ->
+  foldr (\x xs -> In (L (Cons x xs)))
+        (In (L (fmap (In . R . K . fst) f)))
+        vs
 
--- head :: Cata.Alg (ListF x) (Maybe x)
--- head = Cata.Psi $ \f ->
---   case f of
---     Nil      -> Nothing
---     Cons x _ -> Just x
+cons :: x -> Endopara.Algebra (ListF x)
+cons x = prepend [x]
+
+head :: Cata.AlgebraA a (ListF x) (Maybe x)
+head = Cata.Psi (list Nothing (\x _ -> Just x))
 
 -- last :: Cata.Alg (ListF x) (Last x)
 -- last = Cata.Psi $ \f ->
