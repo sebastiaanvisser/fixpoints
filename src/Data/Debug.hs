b0cf8b90a1930c6f4520a10b031f142fa92f9828
Sebastiaan Visser <sfvisser@cs.uu.nl>
2010-05-24 14:31:55 +0200

    Debug annotation.

---
+++ src/Data/Debug.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE FlexibleInstances, FlexibleContexts, MultiParamTypeClasses #-}
+module Data.Debug where
+
+import Control.Monad
+import Data.Annotation
+
+newtype Debug f ix = Debug { unDebug :: f ix }
+
+instance (Functor f, Show (f ())) => In    Debug f IO where inA    = return . Debug <=< printIn
+instance (Functor f, Show (f ())) => Out   Debug f IO where outA   = printOut . unDebug
+instance (Functor f, Show (f ())) => InOut Debug f IO where inOutA = defaultInOut
+
+printIn :: (Functor f, Show (f ())) => f a -> IO (f a)
+printIn f = print ("In: " ++ show (unit f)) >> return f
+
+printOut :: (Functor f, Show (f ())) => f a -> IO (f a)
+printOut f = print ("Out: " ++ show (unit f)) >> return f
+
+unit :: Functor f => f a -> f ()
+unit = fmap (const ())
+
