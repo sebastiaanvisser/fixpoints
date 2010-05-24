4cb8859f82c465736337c0896ee0c203da954d8f
Sebastiaan Visser <sfvisser@cs.uu.nl>
2010-05-24 17:18:22 +0200

    Alg to Algebra rename.

--- src/Data/Morphism/Endoparamorphism.hs
+++ src/Data/Morphism/Endoparamorphism.hs
@@ -16,18 +16,18 @@ import Data.Identity
 import Data.Traversable
 import Prelude hiding (mapM)
 
-data AlgA (a :: (* -> *) -> * -> *) (f :: * -> *) where
-  Psi :: (f (FixA a f, FixA a f) -> FixBotA a f) -> AlgA a f
+data AlgebraA (a :: (* -> *) -> * -> *) (f :: * -> *) where
+  Psi :: (f (FixA a f, FixA a f) -> FixBotA a f) -> AlgebraA a f
 
-type Alg f = forall a. AlgA a f
+type Algebra f = forall a. AlgebraA a f
 
-endoparamorphismMA :: (Monad m, Traversable f, OutIn a f m) => AlgA a f -> FixA a f -> m (FixA a f)
+endoparamorphismMA :: (Monad m, Traversable f, OutIn a f m) => AlgebraA a f -> FixA a f -> m (FixA a f)
 endoparamorphismMA (Psi psi) = topIn . psi <=< mapM (grp (endoparamorphismMA (Psi psi))) <=< outA . out
   where grp f c = liftM (c,) (f c)
 
-endoparamorphismA :: (Traversable f, OutIn a f Identity) => AlgA a f -> FixA a f -> FixA a f
+endoparamorphismA :: (Traversable f, OutIn a f Identity) => AlgebraA a f -> FixA a f -> FixA a f
 endoparamorphismA psi = runIdentity . endoparamorphismMA psi
 
-endoparamorphism :: Traversable f => AlgA Id f -> Fix f -> FixA Id f
+endoparamorphism :: Traversable f => AlgebraA Id f -> Fix f -> FixA Id f
 endoparamorphism psi = runIdentity . endoparamorphismMA psi . fullyInId
 
