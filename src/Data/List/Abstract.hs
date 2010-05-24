02625b62ce1d958fa4eba03cc979678394fceb80
Sebastiaan Visser <sfvisser@cs.uu.nl>
2010-05-24 15:36:34 +0200

    Start of list structure.

---
+++ src/Data/List/Abstract.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE
+    DeriveFunctor
+  , DeriveFoldable
+  , DeriveTraversable
+  , TemplateHaskell
+  , EmptyDataDecls
+  , TypeFamilies
+ #-}
+module Data.List.Abstract where
+
+import Data.Binary
+import Data.Foldable
+import Data.Traversable
+import Data.Fixpoint
+import Generics.Regular hiding (Fix (..))
+import qualified Generics.Regular.Functions.Binary as G
+
+-- List datatype parametrized with value type and recursive positions.
+
+data ListF x f = Nil | Cons x f
+  deriving (Eq, Ord, Functor, Foldable, Traversable)
+
+type List x = Fix (ListF x)
+
+-- Derive generic representation using Regular.
+
+$(deriveAll ''ListF "PFList")
+type instance PF (ListF x f) = PFList x f
+
+-- Binary instance, we get this for free using the generic binary function.
+
+instance (Binary x, Binary f) => Binary (ListF x f) where
+  put = G.gput
+  get = G.gget
+
+-- Smart constructors.
+
+nil :: List x
+nil = In Nil
+
+cons :: x -> List x -> List x
+cons x xs = In (Cons x xs)
+
---
+++ src/Data/List/Algebras.hs
@@ -0,0 +1,53 @@
+module Container.List.Algebras where
+
+-- import Generics.Fixpoint
+-- import qualified Data.Morphism.Anamorphism   as Ana
+-- import qualified Data.Morphism.Apomorphism   as Apo
+-- import qualified Generics.Morphism.Para  as Para
+import Data.List.Abstract
+import Data.Monoid
+import Prelude hiding (tail)
+import qualified Data.Morphism.Catamorphism  as Cata
+
+{-fromList :: Ana.Coalg [x] (ListF x)
+fromList = Ana.Phi $ \f ->
+  case f of
+    []   -> Nil
+    x:xs -> Cons x xs
+
+singleton :: Ana.CoalgA a (Maybe x) (ListF x)
+singleton = Ana.Phi $ \f ->
+  case f of
+    Nothing -> Nil
+    Just x  -> Cons x Nothing-}
+
+length :: Num n => Cata.Alg (ListF t) n
+length = Cata.Psi $ \f ->
+  case f of
+    Nil      -> 0
+    Cons _ n -> 1 + n
+
+{-cons :: x -> Apo.Coendo (ListF x)
+cons v = Apo.Phi $ Cons v . Left-}
+
+head :: Cata.Alg (ListF x) (Maybe x)
+head = Cata.Psi $ \f ->
+  case f of
+    Nil      -> Nothing
+    Cons x _ -> Just x
+
+last :: Cata.Alg (ListF x) (Last x)
+last = Cata.Psi $ \f ->
+  case f of
+    Nil       -> Last Nothing
+    Cons x xs -> Last (Just x) `mappend` xs
+
+-- ini :: Cata.AlgA a (ListF t) (ListF x f)
+-- ini = Cata.Psi $ \f ->
+--   case f of
+--     Nil       -> Nothing
+--     Cons x xs -> fmap 
+
+-- test :: List x -> Maybe x
+-- test = getLast . Cata.cata tail
+
