9dd4dfd196001b7aea38d0267e25e5a0dafa818a
Sebastiaan Visser <sfvisser@cs.uu.nl>
2010-05-27 23:24:03 +0200

    Show instance for Fix.

--- src/Data/Fixpoint.hs
+++ src/Data/Fixpoint.hs
@@ -1,4 +1,11 @@
+{-# LANGUAGE
+    FlexibleContexts
+  , UndecidableInstances
+  #-}
 module Data.Fixpoint where
 
 newtype Fix f = In { out :: f (Fix f) }
+
+instance Show (f (Fix f)) => Show (Fix f) where
+  show (In f) = "{" ++ show f ++ "}"
 
