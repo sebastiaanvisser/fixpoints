935b1835d2cb31d6193ea5ae4ce95342df7c744c
Sebastiaan Visser <sfvisser@cs.uu.nl>
2010-05-29 22:43:18 +0200

    Helper functions for coalgebras.

--- src/Data/Morphism/Endoapomorphism.hs
+++ src/Data/Morphism/Endoapomorphism.hs
@@ -11,6 +11,8 @@ module Data.Morphism.Endoapomorphism where
 import Control.Monad hiding (mapM)
 import Control.Monad.Identity
 import Data.Annotation
+import Data.Constant
+import Data.Sum
 import Data.Fixpoint
 import Data.Identity
 import Data.Traversable
@@ -20,6 +22,15 @@ data CoalgebraA (a :: (* -> *) -> * -> *) (f :: * -> *) where
   Phi :: (f (FixA a f) -> f (Either (FixA a f) (FixBotA a f))) -> CoalgebraA a f
 
 type Coalgebra f = forall a. CoalgebraA a f
+
+make :: f (FixBotA a f) -> Either (FixA a f) (FixBotA a f)
+make = Right . In . L
+
+next :: FixA a f -> Either (FixA a f) (FixBotA a f)
+next = Left
+
+stop :: FixA a f -> Either (FixA a f) (FixBotA a f)
+stop = Right . In . R . K
 
 endoapomorphismMA :: (Monad m, In a f m, Traversable f, OutIn a f m) => CoalgebraA a f -> FixA a f -> m (FixA a f)
 endoapomorphismMA (Phi phi) = outIn1 (mapM (endoapomorphismMA (Phi phi) `either` topIn) . phi)
